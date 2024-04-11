f44f55c501b64e072115b010a42dd53ae1795cd8
BreadDogee <roma.ponomarev.05@mail.ru>
2024-04-11 09:11:47 +0600

    Google Play add

--- app/src/main/java/com/example/labandroid4/FirstActivity.java
+++ app/src/main/java/com/example/labandroid4/FirstActivity.java
@@ -1,13 +1,17 @@
 package com.example.labandroid4;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.Px;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.labandroid4.databinding.ActivityFirstBinding;
+
+import java.net.HttpCookie;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -20,6 +24,7 @@ public class FirstActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,14 +34,29 @@ public class FirstActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            }
+        });
+
+    }
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+                String systemTime = data.getStringExtra("system_time");
+                binding.systemTimeTextview.setText("Системное время: " + systemTime);
+            }
+        }
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
-            String systemTime = data.getStringExtra("system_time");
-            binding.systemTimeTextview.setText("Системное время: " + systemTime);
-        }
-    }
-}
+
